60b15bb26e56961ea2a5c309577ff84a0a05c2f0
jonathantorres <jtorre50@live.spcollege.edu>
2026-04-26 16:22:27 -0500

    Week 3 redo - Exercises 5-1 and 6-1

---
+++ Exercise_6-1/Ch06Ex1RoutingPractice/RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id = "")
+        {
+            if (string.IsNullOrEmpty(id))
+                return Content("No ID supplied.");
+            else
+                return Content("ID: " + id);
+        }
+
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end = 0, string message = "")
+        {
+            string contentString = "Counting down:\n";
+            for (int i = start; i >= end; i--)
+            {
+                contentString += i + "\n";
+            }
+            if (!string.IsNullOrEmpty(message))
+            {
+                contentString += message;
+            }
+            return Content(contentString);
+        }
+    }
+}
