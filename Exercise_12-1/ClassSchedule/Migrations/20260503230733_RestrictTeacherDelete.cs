61a669cefdb021ce7e5af465ba18e8a7b1ff9a48
jonathantorres <jtorre50@live.spcollege.edu>
2026-05-03 18:19:58 -0500

    Week 6 - Exercise 12-1

---
+++ Exercise_12-1/ClassSchedule/Migrations/20260503230733_RestrictTeacherDelete.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassSchedule.Migrations
+{
+    public partial class RestrictTeacherDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
